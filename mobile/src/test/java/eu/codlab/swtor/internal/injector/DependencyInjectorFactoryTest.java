3141e30cc8bee7b394618cf10a472788e96ff3d5
Kevin Le Perf <codlabtech@gmail.com>
2016-02-06 22:08:41 +0100

    update1

--- mobile/src/test/java/eu/codlab/swtor/internal/injector/DependencyInjectorFactoryTest.java
+++ mobile/src/test/java/eu/codlab/swtor/internal/injector/DependencyInjectorFactoryTest.java
@@ -6,35 +6,49 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Created by kevinleperf on 06/02/16.
  */
 public class DependencyInjectorFactoryTest {
 
+    public static void assertUtilityClassWellDefined(final Class<?> clazz)
+            throws NoSuchMethodException, InvocationTargetException,
+            InstantiationException, IllegalAccessException {
+        assertTrue("class must be final",
+                Modifier.isFinal(clazz.getModifiers()));
+        assertEquals("There must be only one constructor", 1,
+                clazz.getDeclaredConstructors().length);
+        final Constructor<?> constructor = clazz.getDeclaredConstructor();
+        if (constructor.isAccessible() ||
+                !Modifier.isPrivate(constructor.getModifiers())) {
+            fail("constructor is not private");
+        }
+        constructor.setAccessible(true);
+        constructor.newInstance();
+        constructor.setAccessible(false);
+        for (final Method method : clazz.getMethods()) {
+            if (!Modifier.isStatic(method.getModifiers())
+                    && method.getDeclaringClass().equals(clazz)) {
+                fail("there exists a non-static method:" + method);
+            }
+        }
+    }
+
     @Test
-    public void testConstructor() {
+    public void testConstructor() throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
         Class<DependencyInjectorFactory> clazz = DependencyInjectorFactory.class;
 
-        assertEquals(0, clazz.getConstructors().length);
-        assertEquals(1, clazz.getDeclaredConstructors().length);
-
-        Constructor<?> constructor = null;
-        try {
-            constructor = clazz.getDeclaredConstructor();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        }
-
-        assertNotNull(constructor);
-        assertFalse(constructor.isAccessible());
-        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+        assertUtilityClassWellDefined(clazz);
     }
 
     @Test
