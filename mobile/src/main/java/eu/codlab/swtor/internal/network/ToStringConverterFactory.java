31805e5211bed1f903389ae90eca20068bb5595c
Kevin Le Perf <codlabtech@gmail.com>
2016-02-06 22:43:14 +0100

    update to string converter factory

--- mobile/src/main/java/eu/codlab/swtor/internal/network/ToStringConverterFactory.java
+++ mobile/src/main/java/eu/codlab/swtor/internal/network/ToStringConverterFactory.java
@@ -70,4 +70,6 @@ public class ToStringConverterFactory extends Converter.Factory {
             }
         };
     }
+
+
 }
--- mobile/src/test/java/eu/codlab/swtor/internal/network/ToStringConverterFactoryTest.java
+++ mobile/src/test/java/eu/codlab/swtor/internal/network/ToStringConverterFactoryTest.java
@@ -1,23 +1,37 @@
 package eu.codlab.swtor.internal.network;
 
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
 
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
-import okio.BufferedSource;
 import retrofit2.Converter;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
  * Created by kevinleperf on 06/02/16.
  */
 public class ToStringConverterFactoryTest {
+
+    @Test
+    public void teststringConverter() {
+        ToStringConverterFactory constructor = new ToStringConverterFactory();
+        Converter converter = constructor.stringConverter(new Type() {
+            @Override
+            public String toString() {
+                return "";
+            }
+        }, new Annotation[]{});
+
+        assertNull(converter);
+    }
 
     @Test
     public void testgetConverterFromString() throws IOException {
