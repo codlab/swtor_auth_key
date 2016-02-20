111cddab969600aba42501c6df63d4e045060add
Kevin Le Perf <codlabtech@gmail.com>
2016-02-20 09:47:32 +0100

    add test on wear

---
+++ wear/src/test/java/eu/codlab/swtor/GenerateCodeTest.java
@@ -0,0 +1,41 @@
+package eu.codlab.swtor;
+
+import org.junit.Test;
+
+import eu.codlab.common.security.CodeProvider;
+import eu.codlab.common.security.CodeProviderFactory;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class GenerateCodeTest {
+    public final static String CODE_OK = "RULAB3CHR3KDHFC3";
+    private final static String CODE_ERROR = "RULAB3CHR3KDHFC";
+
+    @Test
+    public void assertCodeProviderCanBeGenerated() throws Exception {
+
+        CodeProvider provider = CodeProviderFactory.getCodeProvider(CODE_OK);
+        assertNotEquals(null, provider);
+    }
+
+    @Test
+    public void assertCodeProviderGeneratedIsOK() throws Exception {
+        CodeProvider provider = CodeProviderFactory.getCodeProvider(CODE_OK);
+        assertNotEquals(null, provider);
+
+        String generated = provider.generateCode();
+
+        assertNotNull(generated);
+    }
+
+    @Test
+    public void assertCodeProviderCanNotBeGenerated() throws Exception {
+        CodeProvider provider = CodeProviderFactory.getCodeProvider(CODE_ERROR);
+        assertNull(provider);
+    }
+}
